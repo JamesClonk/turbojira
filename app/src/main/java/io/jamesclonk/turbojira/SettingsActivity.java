8bde09542e532f49116981a7064ece1946c44e02
JamesClonk <jamesclonk@jamesclonk.ch>
2016-12-18 19:15:54 +0100

    wip preference screen

--- app/src/main/java/io/jamesclonk/turbojira/SettingsActivity.java
+++ app/src/main/java/io/jamesclonk/turbojira/SettingsActivity.java
@@ -1,13 +1,15 @@
 package io.jamesclonk.turbojira;
 
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        //setContentView(R.layout.activity_settings);
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
