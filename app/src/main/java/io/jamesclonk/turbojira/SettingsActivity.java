e84f2f37a742af64a6bd87407a4521bcbb2f4706
JamesClonk <jamesclonk@jamesclonk.ch>
2016-12-18 19:24:54 +0100

    wip

--- app/src/main/java/io/jamesclonk/turbojira/SettingsActivity.java
+++ app/src/main/java/io/jamesclonk/turbojira/SettingsActivity.java
@@ -1,15 +1,24 @@
 package io.jamesclonk.turbojira;
 
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity
+        implements Preference.OnPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_settings);
-        addPreferencesFromResource(R.xml.preferences);
+        //addPreferencesFromResource(R.xml.settings);
+        Toast.makeText(this, "Test 123!!!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        return false;
     }
 }
